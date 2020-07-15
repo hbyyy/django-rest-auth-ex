af596939590c990c4d925e261e21dcf66c4671e1
hbyyy <qjaduddl94@gmail.com>
2020-07-16 01:07:23 +0900

    google oauth2

--- app/config/urls.py
+++ app/config/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_jwt.views import obtain_jwt_token
 
-from members.views import GoogleLogin, TestView
+from members.views import GoogleLogin, TestJWTView, test_google_view, test_google_token_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,5 +25,7 @@ urlpatterns = [
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('rest-auth/google/', GoogleLogin.as_view(), name='gg_login'),
     path('api-token-auth/', obtain_jwt_token),
-    path('test/', TestView.as_view()),
+    path('testjwt/', TestJWTView.as_view()),
+    path('testgoogle/', test_google_view),
+    path('testgoogle-accesstoken', test_google_token_view)
 ]
--- app/members/views.py
+++ app/members/views.py
@@ -1,5 +1,6 @@
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -14,6 +15,13 @@ class GoogleLogin(SocialLoginView):
     client_class = OAuth2Client
 
 
-class TestView(APIView):
+class TestJWTView(APIView):
     def post(self, request):
         return Response({'user': request.user.username}, status=200)
+
+
+def test_google_view(request):
+    return HttpResponse(f'<p>{dict(request.GET)}<p>')
+
+def test_google_token_view(request):
+    return HttpResponse(f'<p>{dict(request.GET)}<p>')
